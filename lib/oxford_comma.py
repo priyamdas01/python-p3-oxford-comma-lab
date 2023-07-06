002af67136658bcbce16733a6be62ef6386f5cb6
Priyam <12priyamdas34@gmail.com>
2023-07-06 07:19:36 -0400

    complete lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,7 @@
 def oxford_comma(items):
-    return None
+    str = ", ".join(items)
+    if len(items) == 2:
+        str = " and ".join(items)
+    else:
+        str = str.replace(f", {items[-1]}", f", and {items[-1]}")
+    return str
